48f49ebf6eabaa3a30cfc2634b42a492b0cf0bf5
Maneesh Sahu <maneesh@sahu.haus>
2020-03-16 15:44:11 -0700

    Added .config.js

---
+++ src/.config.js
@@ -0,0 +1,19 @@
+export default {
+    org: process.env.VUE_APP_ISSUER.split("/oauth2")[0],
+    oidc: {
+      client_id: process.env.VUE_APP_CLIENT_ID,
+      issuer: process.env.VUE_APP_ISSUER,
+      redirect_uri: process.env.VUE_APP_REDIRECT_URI,
+      o4o_redirect_uri: process.env.VUE_APP_O4O_REDIRECT_URI,
+      scope: process.env.VUE_APP_SCOPES
+    },
+    api: process.env.VUE_APP_API,
+    brand: {
+      logo: process.env.VUE_APP_LOGO,
+      logo_inv: process.env.VUE_APP_LOGO_INVERSE,
+      color: process.env.VUE_APP_MAINCOLOR,
+      name: process.env.VUE_APP_COMPANY_NAME,
+      recaptcha_key: process.env.VUE_APP_RECAPTCHA_SITE_KEY
+    }
+  };
+  
