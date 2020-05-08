5fde27eff8dde6e1e19603bdad69011c11658540
Maneesh Sahu <maneesh@sahu.haus>
2020-05-08 15:37:22 -0700

    Updated to use pkce and passwordless features

--- byob-spa/src/components/Login.vue
+++ byob-spa/src/components/Login.vue
@@ -1,5 +1,4 @@
-<style scoped>
-</style>
+<style scoped></style>
 
 <template>
   <div class="login">
@@ -8,46 +7,52 @@
 </template>
 
 <script>
-import OktaSignIn from '@okta/okta-signin-widget'
-import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
+import OktaSignIn from "@okta/okta-signin-widget";
+import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 
 export default {
-  name: 'Login',
-  mounted: function () {
-    this.$nextTick(function () {
-        const config = {
-          baseUrl: this.$config.oidc.issuer.split('oauth2')[0],
-          clientId: this.$config.oidc.client_id,
-          redirectUri: this.$config.oidc.redirect_uri,
-          authParams: {
-              responseType: 'code',
-              grantType: 'authorization_code',
-              issuer: this.$config.oidc.issuer,
-              scopes: this.$config.oidc.scope.split(' '),
-              display: 'page'
-          }
-        }
-        this.widget = new OktaSignIn(config)
+  name: "Login",
+  mounted: function() {
+    this.$nextTick(function() {
+      const config = {
+        baseUrl: this.$config.oidc.issuer.split("oauth2")[0],
+        clientId: this.$config.oidc.client_id,
+        redirectUri: this.$config.oidc.redirect_uri,
+        authParams: {
+          pkce: true,
+          issuer: this.$config.oidc.issuer,
+          scopes: this.$config.oidc.scope.split(" "),
+          display: "page",
+        },
+        features: {
+          passwordlessAuth: true,
+          webauthn: true,
+          multiOptionalFactorEnroll: true,
+          idpDiscovery: true,
+        },
+      };
+      this.widget = new OktaSignIn(config);
 
-        this.widget.authClient.session.exists()
-        .then((exists)=> {
-          if (exists) {
-            this.$auth.loginRedirect("/", {})
-          } else {
-            this.widget.renderEl(
-                { el: '#okta-signin-container' },
-                (res) => {
-                  console.log(res)
-                },
-                (err) => {throw err}
-            )
-          }
-        })
-    })
+      this.widget.authClient.session.exists().then((exists) => {
+        if (exists) {
+          this.$auth.loginRedirect("/", {});
+        } else {
+          this.widget.renderEl(
+            { el: "#okta-signin-container" },
+            (res) => {
+              console.log(res);
+            },
+            (err) => {
+              throw err;
+            }
+          );
+        }
+      });
+    });
   },
-  destroyed () {
+  destroyed() {
     // Remove the widget from the DOM on path change
-    this.widget.remove()
-  }
-}
+    this.widget.remove();
+  },
+};
 </script>
